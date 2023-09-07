0f4b3c0c3da30973c4f8ffd6f73f244cfeccb688
Gideon Okoroafor <okoro062@umn.edu>
2023-09-06 20:19:43 -0500

    video player

---
+++ video_player/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Gideon Okoroafor on 9/6/23.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo();
+    }
+    
+    private func playVideo(){
+        guard let path = Bundle.main.path(forResource: "vikings_short", ofType: "mp4") else{
+            debugPrint("video not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true){
+            player.play()
+        }
+    }
+
+
+}
+
